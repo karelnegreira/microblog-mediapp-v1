a2ac780140e740e8c875d9df071cb57202bf204a
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-09-07 02:24:56 +1200

    spread btn redirects temp to login page

--- hooks/useRegisterModal.ts
+++ hooks/useRegisterModal.ts
@@ -7,7 +7,7 @@ interface RegisterModalScore {
 }
 
 const useRegisterModal = create<RegisterModalScore>((set) => ({
-    isOpen: true, 
+    isOpen: false, 
     onOpen: () => set({isOpen: true}), 
     onClose: () => set({isOpen: false})
 }));
