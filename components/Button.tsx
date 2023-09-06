d5204b00d6f3c98b4c75196313c401cb1133251c
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-09-06 21:36:02 +1200

    Header, Sidebars already up running no backend functions implemented

---
+++ components/Button.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+interface ButtonProps {
+    label: string;
+    secundary?: boolean;
+    fullWidth?: boolean;
+    large?: boolean;
+    onClick: () => void;
+    disabled?: boolean;
+    outline?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label, 
+    secundary, 
+    fullWidth, 
+    large, 
+    onClick, 
+    disabled, 
+    outline
+}) => {
+  return (
+    <button
+        disabled={disabled}
+        onClick={onClick}
+        className={`
+            disabled: opacity-70
+            disabled: cursor-not-allowed
+            rounded-full
+            font-semibold
+            hover: opacity-80
+            transition
+            border-2
+            ${fullWidth ? 'w-full' : 'w-fit' }
+            ${secundary ? 'bg-white' : 'bg-sky-500'}
+            ${secundary ? 'text-black' : 'text-white'}
+            ${secundary ? 'border-black' : 'border-sky-500'}
+            ${large ? 'text-xl' : 'text-md'}
+            ${large ? 'py-3' : 'py-2'}
+            ${outline ? 'bg-transparent' : ''}
+            ${outline ? 'border-white' : ''}
+            ${outline ? 'text-white' : ''}
+        `}
+    >
+
+    </button>
+  )
+}
+
+export default Button
