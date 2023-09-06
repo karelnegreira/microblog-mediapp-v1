c70031ff7ac15aa948a0f2c5bd3c2bb8168ffc62
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-09-06 23:06:32 +1200

    input componend finished

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-import Model from '@/components/Model'
+
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-     <Model actionLabel="Submit" isOpen title="test model" />
+     
     <Layout>
       <Component {...pageProps} />
     </Layout>
