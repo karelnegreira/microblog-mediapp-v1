451b76511f66826c51d3bf29778294c7e527afdb
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-09-09 23:08:58 +1200

    RegisterModal created

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -3,6 +3,9 @@ import useLoginModal  from '../../hooks/useLoginModal'
 import Input from '../Input';
 import Model from '../Model';
 import useRegisterModal from '@/hooks/useRegisterModal';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 const RegisterModal = () => {
     const loginModal = useLoginModal();
@@ -26,15 +29,28 @@ const RegisterModal = () => {
         try {
             setIsLoading(true);
 
-            /*TODO register and login */
+            await axios.post("/api/register", {
+                email, 
+                password, 
+                username, 
+                name
+            })
+
+            toast.success('Account created successfully! Lets Spread now...')
+
+            signIn('credentials', {
+                email, 
+                password
+            });
 
             registerModal.onClose()
         } catch (error) {
             console.log(error)
+            toast.error('Oh uh, something went wrong :( ')
         } finally {
             setIsLoading(false);
         }
-    }, [registerModal])
+    }, [registerModal, email, password, username, name])
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
