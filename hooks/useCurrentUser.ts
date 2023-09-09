483dfcdb84e2fc19a721241aa2e1790c1a16c442
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-09-09 22:48:46 +1200

    useCurrentUser hook impl with swr lib

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,3 +1,18 @@
 import useSWR from 'swr';
 
+import fetcher from '@/libs/fetcher';
+
+/*it fetches data and stored in its record it checks to see if data should be fetched or not */
+const useCurrentUser = () => {
+    const {data, error, isLoading, mutate} = useSWR('/api/current', fetcher);
+
+    return {
+        data, 
+        error, 
+        isLoading, 
+        mutate
+    }
+};
+
+export default useCurrentUser;
 
