f6cf3af564fbe96da37f9e61a6ed7d49aba747f5
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-10-08 10:09:00 +1200

    Postfeed component setup, PostItem now can see username at posts

---
+++ components/posts/PostItem.tsx
@@ -0,0 +1,71 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useLoginModal from "@/hooks/useLoginModal";
+import { formatDistanceToNowStrict } from "date-fns";
+import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+import Avatar from "../Avatar";
+
+interface PostItemProps {
+    data: Record<string, any>;
+    userId?: string;
+}
+
+const PostItem: React.FC<PostItemProps> = ({data, userId}) => {
+
+    const router = useRouter();
+    const loginModel = useLoginModal();
+    
+    const {data: currentUser} = useCurrentUser();
+
+    const goToUser = useCallback((event: any) => {
+        event.stopPropagation();
+        router.push(`/users/${data.user.id}`)
+    }, [router, data.user.id]);
+
+
+    const goToPosts = useCallback(() => {
+        router.push(`/posts/${data.id}`)
+    }, [router, data.id])
+
+
+    const onLike = useCallback((event: any) => {
+        event.stopPropagation();
+
+        loginModel.onOpen();
+    }, [loginModel])
+
+    const createdAt = useMemo(() => {
+        if (!data?.createdAt) {
+            return null;
+        }
+
+        return formatDistanceToNowStrict(new Date(data.createdAt));
+    }, [data?.createdAt])
+
+
+  return (
+    <div onClick={goToPosts} 
+            className="
+                border-b-[1px] 
+                border-neutral-800 
+                p-5 
+                cursor-pointer
+                hover:bg-neutral-900 
+                transition"
+            >
+        <div className="flex flex-row items-start gap-3">
+            <Avatar userId={data.user.id} />
+            <div>
+                <div className="flex flex-row items-center gap-2">
+                    <p className="text-white font-semibold cursor-pointer hover:underline">{data.user.name}</p>
+                    <span>
+                        @{data.user.username}
+                    </span>
+                </div>
+            </div>
+        </div>        
+    </div>
+  );
+}
+
+export default PostItem
