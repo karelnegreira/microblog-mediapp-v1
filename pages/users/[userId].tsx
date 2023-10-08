a92f6f3611320ad7e4fa99d91877d95262e1ea86
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-10-08 16:03:25 +1200

    follow.ts is implemented

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -5,6 +5,7 @@ import { ClipLoader } from "react-spinners";
 import useUser from "@/hooks/useUser";
 import UserHero from "@/components/users/UserHero";
 import UserBio from "@/components/users/UserBio";
+import PostFeed from "@/components/posts/PostFeed";
 
 const UserView = () => {
     const router = useRouter();
@@ -26,6 +27,7 @@ const UserView = () => {
             <Header showBackArrow label={fetchedUser?.name} />
             <UserHero userId={userId as string} />
             <UserBio userId={userId as string} />
+            <PostFeed userId={userId as string} />
         </>
     );
 }
