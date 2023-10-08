33f895045b593dc228f7399d0d13b89287ec4bca
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-10-09 10:40:41 +1200

    usePost hook created

---
+++ hooks/usePost.ts
@@ -0,0 +1,19 @@
+import useSWR from 'swr';
+
+import fetcher from '@/libs/fetcher';
+
+/*it fetches data and stored in its record it checks to see if data should be fetched or not */
+const usePost = (postId?: string) => {
+    const url = postId ?  `/api/posts/${postId}`: null;
+
+    const {data, error, isLoading, mutate} = useSWR(url, fetcher);
+
+    return {
+        data, 
+        error, 
+        isLoading, 
+        mutate
+    }
+};
+
+export default usePost;
