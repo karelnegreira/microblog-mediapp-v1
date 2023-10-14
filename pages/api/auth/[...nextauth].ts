91a346a56c9cd5bd0172ba1a8c1bfb42ccd29f54
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-10-14 17:11:42 +1200

    back to development env

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -46,7 +46,7 @@ export const authOptions: AuthOptions = {
     ],
     debug: process.env.NODE_ENV === 'development',
     session: {
-      strategy: 'jwt',
+      strategy: 'jwt', 
     },
     jwt: {
       secret: process.env.NEXTAUTH_JWT_SECRET,
