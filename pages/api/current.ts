3d295732840f32e6de502677cb60bda66ca16387
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-10-16 21:42:35 +1200

    removed env variables and hardcoded the urls and tokens, test only

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -1,6 +1,7 @@
-import serverAuth from "@/libs/serverAuth";
+
 import { NextApiRequest, NextApiResponse } from "next";
 
+import serverAuth from "@/libs/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
@@ -8,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     try {
-        const  {currentUser}   = await serverAuth(req, res);
+        const  { currentUser }  = await serverAuth(req, res);
 
         return res.status(200).json(currentUser);
     } catch (error) {
